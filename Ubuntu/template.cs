3c74b77031c7e4775c4c8a828a6539c5b3709ea5
hu-kh <7ussein.5alil@gmail.com>
2019-12-11 12:37:48 -0500

    template updated

--- Ubuntu/template.cs
+++ Ubuntu/template.cs
@@ -2,24 +2,12 @@ using System;
 
 namespace MainProgramm
 {
-<<<<<<< HEAD
-class Program
-{
-static void Main(string[] args)
-{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-    Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
 }
-  
-=======
-   class Program
-   {
-      static void Main(string[] args)
-      {
-
-        Console.ReadKey();
-       }
-   } 
->>>>>>> fb4b0cb774ecb22568aab3e7d3bc4edd54250e8e
-}
-
